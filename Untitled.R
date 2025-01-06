1d5e4f4d94532e5dea9176163c4e05ee83aacf82
Achraf Cohen <acohen@HM-FC-ACOHEN.local>
2025-01-06 10:20:43 -0600

    update

--- Untitled.R
+++ Untitled.R
@@ -1,16 +1,30 @@
-res %>% 
+res = read.csv("MS_wavelet_reserach/final_result.csv") %>% 
+  mutate(wsize= 2^J.Setting) |> 
   mutate(accuracy= accuracy_test, 
-         proc=Preprocess) %>% 
-  mutate(proc = recode(proc, preprocess1 = 'PROC1', 
+         proc=Preprocess,
+         proc = recode(proc, preprocess1 = 'PROC1', 
                        preprocess2 = 'PROC2',
                        preprocess3 = 'PROC3',
-                       preprocess4 = 'PROC4')) %>% 
-  select(model,precision, recall, F1.score, accuracy, proc) %>%
-  
+                       preprocess4 = 'PROC4'),
+         Oversample = recode(Oversample, 
+                             "0" = 'Nosampling', 
+                             "1" = 'Upsampling'),
+         across(where(is.character),as.factor)) |> 
+  select(res_id, model, wavelet, Oversample, proc, wsize, precision, recall, F1.score, accuracy) 
+
+
+
+res |> 
+  select(model, proc, precision, recall, F1.score, accuracy) |> 
   pivot_longer(cols=-c(model,proc), names_to = "metric", values_to = "value") %>% 
-  ggplot(aes(x=proc,y=value, col=metric)) +
-  geom_point()+
+  ggplot(aes(x=proc,y=value, col=model,fill=metric)) +
+  #geom_point()+
+  stat_summary(geom = "point",
+               fun = mean,
+               size=4)+
+  labs(y= "Average value")+
   theme_apa()
+  #facet_wrap(~metric)
 
 res %>% 
   select(model,precision, recall, F1.score, accuracy_test, Preprocess) %>%
